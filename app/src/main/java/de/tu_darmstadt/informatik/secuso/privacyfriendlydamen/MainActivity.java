2693ae4581706ccef7a1c26e3a8ed7dc9024c993
Maksim Melnik <melnik.maksim@hotmail.com>
2017-11-08 13:50:57 +0100

    Recreate fresh project from scratch.

---
+++ app/src/main/java/de/tu_darmstadt/informatik/secuso/privacyfriendlydamen/MainActivity.java
@@ -0,0 +1,13 @@
+package de.tu_darmstadt.informatik.secuso.privacyfriendlydamen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
