effce75792721cf21e56841f8dc2377f42cdb520
Christopher Beckmann <chris.beckm@gmail.com>
2019-11-05 12:28:08 +0100

    Fixed difficulty strings. Fixed some view null pointers. Fixed double layout calls. Version 1.2

--- app/src/main/java/org/secuso/privacyfriendlydame/ui/CheckersLayout.java
+++ app/src/main/java/org/secuso/privacyfriendlydame/ui/CheckersLayout.java
@@ -223,18 +223,21 @@ public class CheckersLayout extends TableLayout {
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                ViewGroup.LayoutParams params = cells[i][j].getLayoutParams();
-                if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-                    params.width = (right - left) / 8;
-                    params.height = (right - left) / 8;
 
-                } else {
-                    params.width = (bottom - top) / 8;
-                    params.height = (bottom - top) / 8;
+        if(changed) {
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    ViewGroup.LayoutParams params = cells[i][j].getLayoutParams();
+                    if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                        params.width = (right - left) / 8;
+                        params.height = (right - left) / 8;
+
+                    } else {
+                        params.width = (bottom - top) / 8;
+                        params.height = (bottom - top) / 8;
+                    }
+                    //cells[i][j].setLayoutParams(params);
                 }
-                cells[i][j].setLayoutParams(params);
             }
         }
     }
