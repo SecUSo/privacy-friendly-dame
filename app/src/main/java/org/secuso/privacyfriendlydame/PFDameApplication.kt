9312543545aee013e88b817f57a61333163fad0c
Patrick Schneider <urulu@student.kit.edu>
2025-10-25 18:34:55 +0200

    Adds and updates required application classes. Migrates preferences. Removes old preference manager class.

--- app/src/main/java/org/secuso/privacyfriendlydame/PFDameApplication.kt
+++ app/src/main/java/org/secuso/privacyfriendlydame/PFDameApplication.kt
@@ -1,20 +1,20 @@
 package org.secuso.privacyfriendlydame
 
 
-import android.app.Application
+import android.app.Activity
 import android.util.Log
 import androidx.work.Configuration
-import org.secuso.privacyfriendlybackup.api.pfa.BackupManager
-import org.secuso.privacyfriendlydame.backup.BackupCreator
-import org.secuso.privacyfriendlydame.backup.BackupRestorer
+import org.secuso.pfacore.ui.PFApplication
+import org.secuso.pfacore.ui.PFData
+import org.secuso.privacyfriendlydame.ui.MainActivity
 
-class PFDameApplication : Application(), Configuration.Provider {
-
-    override fun onCreate() {
-        super.onCreate()
-        BackupManager.backupCreator = BackupCreator()
-        BackupManager.backupRestorer = BackupRestorer()
-    }
+class PFDameApplication : PFApplication() {
+    override val name: String
+        get() = getString(R.string.app_name)
+    override val data: PFData
+        get() = PFApplicationData.instance(this).data
+    override val mainActivity: Class<out Activity>
+        get() = MainActivity::class.java
 
     override val workManagerConfiguration = Configuration.Builder().setMinimumLoggingLevel(Log.INFO).build()
 }
